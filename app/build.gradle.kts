3db8984f4307be81ea170d5739b3136fa4c298e1
Kavita Patil <kavitamp19@gmail.com>
2023-12-13 21:46:03 +0100

    Added MainActivity and base configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 android {
-    namespace = "com.amangarg.searchview_coroutines_flow"
-    compileSdk = 33
+    namespace = "com.amangarg.searchview.coroutines.flow"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.amangarg.searchview_coroutines_flow"
+        applicationId = "com.amangarg.searchview.coroutines.flow"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -40,6 +40,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
